593f8c07d5cf9613ef56a82b820953d80a0a1243
Mikaela Olsson <Mikaela.Olsson@xlent.se>
2023-08-24 18:54:08 +0200

    cleaning

--- GameX.Web/wwwroot/build/scripts/menu-toggle.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const menuItems = document.querySelector('#menu .menu-items');
-const menuToggle = document.querySelector('#menu .menu-toggle');
-menuToggle === null || menuToggle === void 0 ? void 0 : menuToggle.addEventListener('click', function () {
-    menuItems === null || menuItems === void 0 ? void 0 : menuItems.classList.toggle('show');
-});
